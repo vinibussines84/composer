523386c410a7a7172d6f559c9f7ad0b80e3de898
Vinicius Bussines <emprearialgrotwh@gmail.com>
2025-05-07 13:43:43 -0300

    293

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Filament\Facades\Filament;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,11 +18,16 @@ class AppServiceProvider extends ServiceProvider
             $this->app->register(TelescopeServiceProvider::class);
         }
     }
+
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        Filament::serving(function () {
+            Filament::getPanel('admin_panel')?->auth()->checkAccessUsing(function (User $user) {
+                return $user->dashboard_access === 3;
+            });
+        });
     }
 }
