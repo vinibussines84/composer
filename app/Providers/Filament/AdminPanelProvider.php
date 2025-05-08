a441d32e12c99aa1cd5bcddf691264e30a9c3ca4
Vinicius Bussines <emprearialgrotwh@gmail.com>
2025-05-08 09:47:51 -0300

    login

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Auth\Login;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -26,6 +27,8 @@ use Awcodes\LightSwitch\Enums\Alignment;
 use App\Filament\Widgets\AdminStats;
 use App\Filament\Widgets\Ultimas10TransacoesDoUsuario;
 
+
+
 class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
@@ -34,7 +37,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('trust')
             ->default()
             ->path('trust')
-            ->login()
+            ->login(Login::class)
             ->registration()
             ->passwordReset()
             ->profile()
