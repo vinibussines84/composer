f7c4303bd41a651b063066b30a2141a66105f1b8
Vinicius Bussines <emprearialgrotwh@gmail.com>
2025-05-08 18:36:56 -0300

    ok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Filament\Facades\Filament;
 use App\Models\User;
 
@@ -24,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // 👇 Força HTTPS em produção
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Filament::serving(function () {
             $panel = Filament::getCurrentPanel();
 
