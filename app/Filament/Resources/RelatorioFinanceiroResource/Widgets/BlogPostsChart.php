ddb5bea70617a01cbb0779720577d01e1785c02a
Vinicius Bussines <emprearialgrotwh@gmail.com>
2025-05-14 20:51:54 -0300

    as

--- app/Filament/Resources/RelatorioFinanceiroResource/Widgets/BlogPostsChart.php
+++ app/Filament/Resources/RelatorioFinanceiroResource/Widgets/BlogPostsChart.php
@@ -12,7 +12,7 @@ class BlogPostsChart extends ChartWidget
 {
     protected static ?string $heading = 'Faturamento por período';
 
-    protected int | string | array $columnSpan = 6;
+    protected int | string | array $columnSpan = 4;
 
     public ?string $startDate = null;
     public ?string $endDate = null;
