f94bd937e0b60b880d8aa6287e7fe891b52a802a
Vinicius Bussines <emprearialgrotwh@gmail.com>
2025-06-18 15:21:04 -0300

    line

---
+++ app/Filament/Vink/Resources/ClientResource/Pages/CreateClient.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Vink\Resources\ClientResource\Pages;
+
+use App\Filament\Vink\Resources\ClientResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateClient extends CreateRecord
+{
+    protected static string $resource = ClientResource::class;
+}
---
+++ app/Filament/Vink/Resources/ClientResource/Pages/EditClient.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Vink\Resources\ClientResource\Pages;
+
+use App\Filament\Vink\Resources\ClientResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditClient extends EditRecord
+{
+    protected static string $resource = ClientResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
