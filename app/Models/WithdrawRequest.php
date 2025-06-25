76b00150952642851bd31c3d2f097b3f3e307cad
Vinicius Bussines <emprearialgrotwh@gmail.com>
2025-06-25 11:22:19 -0300

    films

--- app/Models/WithdrawRequest.php
+++ app/Models/WithdrawRequest.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class WithdrawRequest extends Model
 {
-    // código do modelo WithdrawRequest aqui
+    protected $fillable = [
+        'user_id',    // Permite atribuição em massa para user_id
+        'amount',
+        'pix_type',
+        'pix_key',
+        'status',
+    ];
+
+    // Relacionamento com o usuário
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
