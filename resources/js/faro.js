2d92a33ebf82a1f0740dfed2a0165ef5494466d9
Vinicius Bussines <emprearialgrotwh@gmail.com>
2025-07-10 21:00:52 -0300

    fix

--- resources/js/faro.js
+++ resources/js/faro.js
@@ -1,15 +1,19 @@
-import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk'
-import { TracingInstrumentation } from '@grafana/faro-web-tracing'
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+  import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 
-initializeFaro({
-  url: 'https://faro-collector-prod-sa-east-1.grafana.net/collect/60068c3cdb87b8f16581efb184df7bab',
-  app: {
-    name: 'Ok',
-    version: '1.0.0',
-    environment: 'production',
-  },
-  instrumentations: [
-    ...getWebInstrumentations(),
-    new TracingInstrumentation(),
-  ],
-});
+  initializeFaro({
+    url: 'https://faro-collector-prod-sa-east-1.grafana.net/collect/63ae8be6d096c025d83aa8a924d2912e',
+    app: {
+      name: 'Trust',
+      version: '1.0.0',
+      environment: 'production'
+    },
+    
+    instrumentations: [
+      // Mandatory, omits default instrumentations otherwise.
+      ...getWebInstrumentations(),
+
+      // Tracing package to get end-to-end visibility for HTTP requests.
+      new TracingInstrumentation(),
+    ],
+  });
